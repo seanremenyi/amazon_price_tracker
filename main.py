84900ee3606702424f0cef441dc9caf9d668d7ce
Sean Remenyi <sean.remenyi@gmail.com>
2021-07-18 16:35:10 +1000

    work in progress

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+import lxml
+
+url = "https://www.amazon.com/Duo-Evo-Plus-esterilizadora-vaporizador/dp/B07W55DDFB/ref=sr_1_4?qid=1597660904"
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "lxml")
+print(soup.prettify())
+
+price = soup.find(id="").get_text()
 
